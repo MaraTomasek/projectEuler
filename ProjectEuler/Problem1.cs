179fd67c544b8cdf219b4d6afefc50c5081426fe
Mara Tomasek <mara.tomasek@gmx.de>
2022-02-15 15:16:25 +0100

    changed problems to inherit from problem base class

--- ProjectEuler/Problem1.cs
+++ ProjectEuler/Problem1.cs
@@ -1,7 +1,7 @@
 namespace ConsoleApp1;
 
-class Problem1 {
-    int Main() {
+class Problem1 : Problem {
+    public override int Main() {
         /* Multiples of 3 or 5
                 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
                 The sum of these multiples is 23.
@@ -14,7 +14,6 @@ class Problem1 {
         for (int i = 0; i < n; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
                 sum += i;
-                continue;
             }
         }
 
