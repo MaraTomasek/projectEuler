43e9da915ede9e627fad051fa3d4e3aabc4498f2
Mara Tomasek <mara.tomasek@gmx.de>
2022-02-15 12:46:20 +0100

    Changed Namespace in Problem1 to fit Project

--- ProjectEuler/Problem1.cs
+++ ProjectEuler/Problem1.cs
@@ -1,7 +1,7 @@
-namespace ProjectEuler;
+namespace ConsoleApp1;
 
 class Problem1 {
-    int main(string[] args) {
+    int Main() {
         /* Multiples of 3 or 5
                 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
                 The sum of these multiples is 23.
@@ -10,7 +10,7 @@ class Problem1 {
 
         const int n   = 1000;
         int       sum = 0;
-            
+
         for (int i = 0; i < n; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
                 sum += i;
