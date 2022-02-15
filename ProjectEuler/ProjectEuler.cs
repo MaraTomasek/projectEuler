9e4700af7670d7743a712ec77a27d8cb257e25da
Mara Tomasek <mara.tomasek@gmx.de>
2022-02-15 15:17:51 +0100

    Made main function static added menu for problem 3

--- ProjectEuler/ProjectEuler.cs
+++ ProjectEuler/ProjectEuler.cs
@@ -1,20 +1,23 @@
-using System.Collections.Specialized;
-
 namespace ConsoleApp1;
 
-class ProjectEuler {
-    public int Main(int projectNumber) {
-        int[] completedProblems = new int[] {1, 2};
+static class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("hi?");
+        int   projectNumber     = 2;
+        int[] completedProblems = new int[] {1, 2, 3};
 
         if (completedProblems.Contains(projectNumber)) {
-            Console.Write("Solution to Problem {0}: {1}", projectNumber, runProblem(projectNumber));
+            Console.WriteLine("Solution to Problem {0}: {1}", projectNumber, RunProblem(projectNumber));
+        }
+        else {
+            Console.WriteLine("No solution programmed for this exercise.");
         }
 
-        return 0;
+        Console.Read();
     }
 
-    private int runProblem(int projectNumber) {
-        Problem p = new Problem();
+    private static int RunProblem(int projectNumber) {
+        Problem p = null;
 
         switch (projectNumber) {
             case 1:
@@ -23,8 +26,14 @@ class ProjectEuler {
             case 2:
                 p = new Problem2();
                 break;
+            case 3:
+                p = new Problem3();
+                break;
         }
 
-        return p.Main();
+        if (p != null) {
+            return p.Main();
+        }
+        return 0;
     }
 }
