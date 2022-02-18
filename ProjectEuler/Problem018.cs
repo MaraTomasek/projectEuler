8a4a493c46b568f3902732f43a23cdd1a4224d89
Mara Tomasek <mara.tomasek@gmx.de>
2022-02-19 00:25:28 +0100

    Removed some comments

--- ProjectEuler/Problem018.cs
+++ ProjectEuler/Problem018.cs
@@ -76,13 +76,12 @@ public class Problem18 : Problem {
                 triangleInt[i, j] = Convert.ToInt32(line[j]);
             }
         }
-
+        
         for (int i = 0; i < width; i++) {
             pathSum[height - 1, i] = triangleInt[height - 1, i];
         }
 
-        //Iterate bottom-to-top
-        for (int btt = height - 1; btt >= 0; btt--) {
+        for (int btt = height - 2; btt >= 0; btt--) {
             for (int ltr = 0; ltr < btt; ltr++) {
                 pathSum[btt, ltr] = triangleInt[btt, ltr] + Math.Max(pathSum[btt + 1, ltr], pathSum[btt + 1, ltr + 1]);
             }
